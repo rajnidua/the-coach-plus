d67d6101514c2090fb63f43ccb204505e824b8a5
Rajni Dua <rajni.dua14@gmail.com>
2021-11-07 09:48:42 +1030

    welcome page file

---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/navbar/navbar";
+import Body from "../components/welcome-body/body";
+import Footer from "../components/footer/footer";
+
+function Welcome() {
+  return (
+    <div>
+      <NavBar />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default Welcome;
