c3d297a0997c89894a286f2cb367f35921a528f1
Rajni Dua <rajni.dua14@gmail.com>
2021-11-13 08:54:37 +1030

    formatted the session start date

--- server/models/Coach.js
+++ server/models/Coach.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const User = require("./User");
 const { Schema } = mongoose;
+const dateFormat = require("../utils/dateFormat");
 
 const coachSchema = new Schema({
   userProfile: [
@@ -40,6 +41,7 @@ const coachSchema = new Schema({
     type: Date,
     required: false,
     default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
   },
   days: {
     type: [String],
